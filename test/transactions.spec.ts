1f073330cb65d0a6d215398960e2193f152f51e2
Matheus Moreira <matheustjp111@gmail.com>
2025-01-20 09:06:52 -0300

    chore: setup db test for tests

--- test/transactions.spec.ts
+++ test/transactions.spec.ts
@@ -1,6 +1,15 @@
-import { afterAll, beforeAll, describe, expect, it, test } from 'vitest'
+import {
+  afterAll,
+  beforeAll,
+  beforeEach,
+  describe,
+  expect,
+  it,
+  test,
+} from 'vitest'
 import request from 'supertest'
 import { app } from '../src/app'
+import { execSync } from 'node:child_process'
 
 describe('Transactions routes', () => {
   beforeAll(async () => {
@@ -9,6 +18,11 @@ describe('Transactions routes', () => {
 
   afterAll(async () => {
     await app.close()
+  })
+
+  beforeEach(() => {
+    execSync('npm run knex migrate:rollback --all')
+    execSync('npm run knex migrate:latest')
   })
 
   it('should be able to create a new transaction', async () => {
